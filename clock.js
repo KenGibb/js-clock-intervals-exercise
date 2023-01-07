1519d7cf4d6c8e9d4f80b2ab997dd17dd0a17ece
KenGibb <kenjibbo@gmail.com>
2023-01-07 17:32:08 -0600

    finished js clock

---
+++ clock.js
@@ -0,0 +1,23 @@
+let secondHand = document.getElementById('second')
+let hourHand = document.getElementById('hour')
+let minuteHand = document.getElementById('minute')
+const currentTime = () => {
+    let date = new Date()
+    let currentHour = date.getHours()
+    let currentMin = date.getMinutes()
+    let currentSec = date.getSeconds()
+    //console.log(currentHour)
+    let time = (`Hour${currentHour}:Minute${currentMin}:Seconds${currentSec} `) 
+    secondHand.style.transform = 'rotate('+ (currentSec * 6) + 'deg)'
+    minuteHand.style.transform = 'rotate('+ (currentMin * 6) + 'deg)'
+    hourHand.style.transform = 'rotate('+ (currentHour * 30) + 'deg)'
+    console.log(`The current time is ${time}`)
+}
+setInterval(currentTime, 1000)
+
+
+
+
+
+
+
